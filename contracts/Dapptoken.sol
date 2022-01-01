67a0ecab5714d228d12c2a94924d5be5c7ac159d
prem <prem.ch@ku.th>
2022-01-01 12:34:37 +0700

    fix: Change token mint

--- contracts/Dapptoken.sol
+++ contracts/Dapptoken.sol
@@ -5,6 +5,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract DappToken is ERC20 {
     constructor() public ERC20("Dapp token", "DAPP") {
-        _mint(msg.sender, 1e18);
+        _mint(msg.sender, 1000e21);
     }
 }
